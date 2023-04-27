1e10e40d73470a12f35b7cfb3c703e794b505ad9
Somer Lillard <slillard@unc.edu>
2023-04-27 17:10:30 -0400

    Attempt #1 PuzzleImpl Attempt #5 ModelImpl Handgrade #1

--- src/main/java/com/comp301/a09akari/view/MessageView.java
+++ src/main/java/com/comp301/a09akari/view/MessageView.java
@@ -11,41 +11,38 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
 public class MessageView implements FXComponent, ModelObserver {
-    private Model model;
-    private ClassicMvcController controller;
+  private Model model;
+  private ClassicMvcController controller;
 
-    public MessageView(Model model, ClassicMvcController controller){
-        if(model != null && controller != null){
-            this.model = model;
-            this.controller = controller;
-        }
-        else{
-            throw new IllegalArgumentException();
-        }
+  public MessageView(Model model, ClassicMvcController controller) {
+    if (model != null && controller != null) {
+      this.model = model;
+      this.controller = controller;
+    } else {
+      throw new IllegalArgumentException();
     }
+  }
 
-    @Override
-    public Parent render() {
-        VBox pane = new VBox();
-        pane.getChildren().clear();
-        pane.setAlignment(Pos.CENTER);
-        Label title = new Label("Akari");
-        title.setFont(new Font("Arial", 30));
-        Label PuzzleIndex = new Label("Puzzle: " + (Integer.toString(model.getActivePuzzleIndex() + 1)));
-        PuzzleIndex.setFont(new Font("Arial", 10));
-        pane.getChildren().add(title);
-        pane.getChildren().add(PuzzleIndex);
-        Label win = new Label("You win! Congrats!");
-    if (model.isSolved()){
-        pane.getChildren().add(win);
+  @Override
+  public Parent render() {
+    VBox pane = new VBox();
+    pane.getChildren().clear();
+    pane.setAlignment(Pos.CENTER);
+    Label title = new Label("Akari");
+    title.setFont(new Font("Arial", 30));
+    Label PuzzleIndex =
+        new Label("Puzzle: " + (Integer.toString(model.getActivePuzzleIndex() + 1)));
+    PuzzleIndex.setFont(new Font("Arial", 10));
+    pane.getChildren().add(title);
+    pane.getChildren().add(PuzzleIndex);
+    Label win = new Label("You win! Congrats!");
+    if (model.isSolved()) {
+      pane.getChildren().add(win);
     }
-        return pane;
-    }
+    return pane;
+  }
 
-
-    // ignore for now
-    @Override
-    public void update(Model model) {
-
-    }
+  // ignore for now
+  @Override
+  public void update(Model model) {}
 }
